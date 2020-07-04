e9db17f86101756a6990593032c3e9965b6a0eaf
Wagner <wg.o.costa@gmail.com>
2020-07-04 16:27:46 -0300

    Exemplo de diretiva estrutural

--- frontend/src/app/directives/for.directive.ts
+++ frontend/src/app/directives/for.directive.ts
@@ -1,17 +1,24 @@
-import { Directive, OnInit, Input } from '@angular/core';
+import { Directive, OnInit, Input, ViewContainerRef, TemplateRef } from '@angular/core';
 
 @Directive({
   selector: '[myFor]'
 })
 export class ForDirective implements OnInit {
 
-  @Input('myForEm') numbers: number[]
-  @Input('myForusando') texto: string
-  constructor() {
+  @Input('myForBrasil') numbers: number[];
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+
+  ) {
     console.log('MyFor')
   }
   ngOnInit(): void {
-    console.log(this.texto)
+    for (let number of this.numbers) {
+      // this.container.createEmbeddedView(
+      //  this.template, { $implicit: number });
+    }
+
     console.log(this.numbers)
   }
 }
