956ffb1b1850d326ce253a3a333dc0c2931fe11b
Wagner <wg.o.costa@gmail.com>
2020-07-28 20:39:05 -0300

    Ajuste no delete

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -25,7 +25,7 @@ export class ProductDeleteComponent implements OnInit {
   }
 
   deleteProduct(): void {
-    this.productService.delete(this.product._id).subscribe((product) => {
+    this.productService.delete(this.product._id).subscribe(() => {
       this.productService.showMessage("Producto excluido com sucesso!");
       this.router.navigate(["/products"]);
     });
