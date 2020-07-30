331905c938c63f535f8a72a54dae24b2dc00313f
Wagner <wg.o.costa@gmail.com>
2020-07-30 08:03:18 -0300

    Removendo pasta

--- herokubackend/src/app/controller/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-//ler todos os arquivos do diretório que são controllers
-module.exports = (app) => {
-  fs.readdirSync(__dirname)
-    .filter((file) => file.indexOf(".") !== 0 && file !== "index.js")
-    .forEach((file) => require(path.resolve(__dirname, file))(app));
-};
